99bc841becb80c12137b9eb8704c45b34ef3904d
Luis Reynoso Perez <luisr2165@icloud.com>
2024-04-29 23:05:38 -0700

    added boggle solver function (if necessary)

--- search.py
+++ search.py
@@ -1307,6 +1307,28 @@ cubes16 = ['FORIXB', 'MOQABJ', 'GURILW', 'SETUPL',
            'ANEDVZ', 'PINESH', 'ABILYT', 'GKYLEU']
 
 
+# boggle solver function; not needed at the moment
+def boggle_hill_climbing(board=None, ntimes=100, verbose=True):
+    """Solve inverse Boggle by hill-climbing: find a high-scoring board by
+    starting with a random one and changing it."""
+    finder = BoggleFinder()
+    if board is None:
+        board = random_boggle()
+    best = len(finder.set_board(board))
+    for _ in range(ntimes):
+        i, oldc = mutate_boggle(board)
+        new = len(finder.set_board(board))
+        if new > best:
+            best = new
+            if verbose:
+                print(best, _, board)
+        else:
+            board[i] = oldc  # Change back
+    if verbose:
+        print_boggle(board)
+    return board, best
+
+
 def random_boggle(n=4):
     """Return a random Boggle board of size n x n.
     We represent a board as a linear list of letters."""
